c237a2f5860ac23633138a5e84e8f8423c1686e5
sudevE <sudev.duple@gmail.com>
2023-08-07 11:31:33 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { ReactSVG } from 'react-svg';
+import PostCreate from "./PostCreate";
+
+export default () => {
+    return (<div>
+      <h1>Post Create</h1>
+      <PostCreate/>
+    </div>)
+}
